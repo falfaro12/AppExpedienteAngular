4b9134cce55362307e3aa83e1af7ac26a4dc3736
falfaro12 <fabi.alfaro988@gmail.com>
2019-08-04 18:16:30 -0600

    dasda

---
+++ src/app/share/helpers/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  CanActivate,
+  Router
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthenticationService } from "../authentication.service";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const currentUser = this.authenticationService.currentUserValue;
+    if (currentUser) {
+      // iniciado sesión, devuelve verdadero
+      return true;
+    }
+
+    // no logueado,redirigir a la página de inicio de sesión con la URL de retorno
+    this.router.navigate(["usuario/login"], {
+      queryParams: { returnUrl: state.url }
+    });
+    return false;
+  }
+}
---
+++ src/app/share/helpers/error.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { AuthenticationService } from "../authentication.service";
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private authenticationService: AuthenticationService) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError(err => {
+        if (err.status === 401) {
+          // cierre de sesión automático si la respuesta 401
+          this.authenticationService.logout();
+          location.reload(true);
+        }
+
+        return throwError(err);
+      })
+    );
+  }
+}
